2e5be86f6df69c34533959b72d252108bfd8a059
Khavkin <hdm@ukr.net>
2022-12-04 17:32:49 +0200

    add resoved task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const TIMER_TIMEOUT = 1000;
+let timerId;
 
+const onBtnStartHandler = event => {
+  btnStart.disabled = true;
+  body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, TIMER_TIMEOUT);
+};
+
+const onBtnStopHandler = event => {
+  btnStart.disabled = false;
+  clearInterval(timerId);
+};
+
+btnStart.addEventListener('click', onBtnStartHandler);
+btnStop.addEventListener('click', onBtnStopHandler);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
